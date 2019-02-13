3c2a1a9ec97710ee852002f70fe89ab133120a18
talus005 <expogrow.org@gmail.com>
2019-02-13 20:14:49 +0530

    Initial commit

---
+++ src/main/java/com/training/mjunction/zuulgateway/Application.java
@@ -0,0 +1,24 @@
+package com.training.mjunction.zuulgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@EnableDiscoveryClient
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(final SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+
+	public static void main(final String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
