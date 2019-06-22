54ab4dd164f00eee92d6619bb36c3aa0e4802ece
talus005 <sanjib.x.talukdar.-nd@disney.com>
2019-06-23 00:16:08 +0530

    Fixed issues

--- src/main/java/com/training/gateway/zuulgateway/config/DynamicOauth2ClientContextFilter.java
+++ src/main/java/com/training/gateway/zuulgateway/config/DynamicOauth2ClientContextFilter.java
@@ -1,48 +1,47 @@
-package com.training.gateway.zuulgateway.config;
-
-import java.io.IOException;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
-import org.springframework.security.oauth2.client.resource.UserRedirectRequiredException;
-import org.springframework.security.web.DefaultRedirectStrategy;
-import org.springframework.security.web.RedirectStrategy;
-import org.springframework.web.util.UriComponentsBuilder;
-
-public class DynamicOauth2ClientContextFilter extends OAuth2ClientContextFilter {
-
-	private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
-
-	@Override
-	protected void redirectUser(final UserRedirectRequiredException e, final HttpServletRequest request,
-			final HttpServletResponse response) throws IOException {
-
-		final String redirectUri = e.getRedirectUri();
-		final UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(redirectUri);
-		final Map<String, String> requestParams = e.getRequestParams();
-
-		for (final Map.Entry<String, String> param : requestParams.entrySet()) {
-			builder.queryParam(param.getKey(), param.getValue());
-		}
-
-		if (e.getStateKey() != null) {
-			builder.queryParam("state", e.getStateKey());
-		}
-
-		final String url = getBaseUrl(request) + builder.build().encode().toUriString();
-		redirectStrategy.sendRedirect(request, response, url);
-	}
-
-	@Override
-	public void setRedirectStrategy(final RedirectStrategy redirectStrategy) {
-		this.redirectStrategy = redirectStrategy;
-	}
-
-	private String getBaseUrl(final HttpServletRequest request) {
-		final StringBuffer url = request.getRequestURL();
-		return url.substring(0, url.length() - request.getRequestURI().length() + request.getContextPath().length());
-	}
-}
+/*
+ * package com.training.gateway.zuulgateway.config;
+ *
+ * import java.io.IOException; import java.util.Map;
+ *
+ * import javax.servlet.http.HttpServletRequest; import
+ * javax.servlet.http.HttpServletResponse;
+ *
+ * import
+ * org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
+ * import org.springframework.security.oauth2.client.resource.
+ * UserRedirectRequiredException; import
+ * org.springframework.security.web.DefaultRedirectStrategy; import
+ * org.springframework.security.web.RedirectStrategy; import
+ * org.springframework.web.util.UriComponentsBuilder;
+ *
+ * public class DynamicOauth2ClientContextFilter extends
+ * OAuth2ClientContextFilter {
+ *
+ * private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+ *
+ * @Override protected void redirectUser(final UserRedirectRequiredException e,
+ * final HttpServletRequest request, final HttpServletResponse response) throws
+ * IOException {
+ *
+ * final String redirectUri = e.getRedirectUri(); final UriComponentsBuilder
+ * builder = UriComponentsBuilder.fromUriString(redirectUri); final Map<String,
+ * String> requestParams = e.getRequestParams();
+ *
+ * for (final Map.Entry<String, String> param : requestParams.entrySet()) {
+ * builder.queryParam(param.getKey(), param.getValue()); }
+ *
+ * if (e.getStateKey() != null) { builder.queryParam("state", e.getStateKey());
+ * }
+ *
+ * final String url = getBaseUrl(request) +
+ * builder.build().encode().toUriString();
+ * redirectStrategy.sendRedirect(request, response, url); }
+ *
+ * @Override public void setRedirectStrategy(final RedirectStrategy
+ * redirectStrategy) { this.redirectStrategy = redirectStrategy; }
+ *
+ * private String getBaseUrl(final HttpServletRequest request) { final
+ * StringBuffer url = request.getRequestURL(); return url.substring(0,
+ * url.length() - request.getRequestURI().length() +
+ * request.getContextPath().length()); } }
+ */
--- src/main/java/com/training/gateway/zuulgateway/config/RelayTokenFilter.java
+++ src/main/java/com/training/gateway/zuulgateway/config/RelayTokenFilter.java
@@ -1,3 +1,4 @@
+
 package com.training.gateway.zuulgateway.config;
 
 import java.util.Set;
@@ -7,19 +8,30 @@ import org.springframework.stereotype.Component;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 
-// XXX While waiting for:
-// https://github.com/spring-cloud/spring-cloud-netflix/issues/944
+// XXX While waiting for: //
+//https://github.com/spring-cloud/spring-cloud-netflix/issues/944
+
 @Component
 public class RelayTokenFilter extends ZuulFilter {
+
 	@Override
 	public Object run() {
-		final RequestContext ctx = RequestContext.getCurrentContext();
 
-		// Alter ignored headers as per:
+		final RequestContext currentContext = RequestContext.getCurrentContext();
+
+		// currentContext.addZuulRequestHeader("host",
+		// currentContext.getRequest().getHeader("host"));
+
+		// currentContext.getZuulRequestHeaders().remove("x-forwarded-prefix");
+
+		// Alter ignored headers as per: // //
 		// https://gitter.im/spring-cloud/spring-cloud?at=56fea31f11ea211749c3ed22
+
 		@SuppressWarnings("unchecked")
-		final Set<String> headers = (Set<String>) ctx.get("ignoredHeaders");
+		final Set<String> headers = (Set<String>) currentContext.get("ignoredHeaders");
+
 		// We need our JWT tokens relayed to resource servers
+
 		headers.remove("authorization");
 
 		return null;
