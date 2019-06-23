2f5bd0f8f92358fc57152e7e61b6e7728d3cb5fc
talus005 <sanjib.x.talukdar.-nd@disney.com>
2019-06-23 10:31:17 +0530

    Fixed issues

--- src/main/java/com/training/gateway/zuulgateway/Application.java
+++ src/main/java/com/training/gateway/zuulgateway/Application.java
@@ -4,15 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.filters.route.FallbackProvider;
 import org.springframework.context.annotation.Bean;
+
+import com.training.gateway.zuulgateway.config.GenericFallbackProvider;
 
 import brave.sampler.Sampler;
 
 @EnableHystrix
 @EnableZuulProxy
+@EnableCircuitBreaker
 @EnableDiscoveryClient
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
@@ -29,6 +34,41 @@ public class Application extends SpringBootServletInitializer {
 	@Bean
 	public Sampler defaultSampler() {
 		return Sampler.ALWAYS_SAMPLE;
+	}
+
+	@Bean
+	public FallbackProvider uaaSvcsZuulFallbackProvider() {
+		return new GenericFallbackProvider("uaa-svcs");
+	}
+
+	@Bean
+	public FallbackProvider userSvcsZuulFallbackProvider() {
+		return new GenericFallbackProvider("user-svcs");
+	}
+
+	@Bean
+	public FallbackProvider productCatalogZuulFallbackProvider() {
+		return new GenericFallbackProvider("product-catalog");
+	}
+
+	@Bean
+	public FallbackProvider productCompositeZuulFallbackProvider() {
+		return new GenericFallbackProvider("product-composite");
+	}
+
+	@Bean
+	public FallbackProvider pricingSvcsZuulFallbackProvider() {
+		return new GenericFallbackProvider("pricinf-svcs");
+	}
+
+	@Bean
+	public FallbackProvider recommSvcsZuulFallbackProvider() {
+		return new GenericFallbackProvider("recomm-svcs");
+	}
+
+	@Bean
+	public FallbackProvider reviewSvcsZuulFallbackProvider() {
+		return new GenericFallbackProvider("review-svcs");
 	}
 
 }
---
+++ src/main/java/com/training/gateway/zuulgateway/config/GenericFallbackProvider.java
@@ -0,0 +1,62 @@
+package com.training.gateway.zuulgateway.config;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.springframework.cloud.netflix.zuul.filters.route.FallbackProvider;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpResponse;
+
+public class GenericFallbackProvider implements FallbackProvider {
+	private final String route;
+
+	public GenericFallbackProvider(final String route) {
+		this.route = route;
+	}
+
+	@Override
+	public String getRoute() {
+		return route;
+	}
+
+	@Override
+	public ClientHttpResponse fallbackResponse(final String route, final Throwable cause) {
+		return new ClientHttpResponse() {
+			@Override
+			public HttpStatus getStatusCode() throws IOException {
+				return HttpStatus.SERVICE_UNAVAILABLE;
+			}
+
+			@Override
+			public int getRawStatusCode() throws IOException {
+				return HttpStatus.SERVICE_UNAVAILABLE.value();
+			}
+
+			@Override
+			public String getStatusText() throws IOException {
+				return HttpStatus.SERVICE_UNAVAILABLE.toString();
+			}
+
+			@Override
+			public void close() {
+			}
+
+			@Override
+			public InputStream getBody() throws IOException {
+				return new ByteArrayInputStream(
+						("{\"message\":\"Sorry, Service is Down!\",\"exception\":\"" + cause.getMessage() + "\"}")
+								.getBytes());
+			}
+
+			@Override
+			public HttpHeaders getHeaders() {
+				final HttpHeaders headers = new HttpHeaders();
+				headers.setContentType(MediaType.APPLICATION_JSON);
+				return headers;
+			}
+		};
+	}
+}
